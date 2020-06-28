5b9674a340f469ce38d0e95025db33f601b45365
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2020-06-28 16:40:16 -0300

    Fix in risk payload validation

--- app/schemas/insurances.js
+++ app/schemas/insurances.js
@@ -23,6 +23,18 @@ const vehicleSchema = {
     required: [ "year" ],
 };
 
+const riskQuestionsSchema = {
+    id: "/RiskQuestions",
+    type: "array",
+    minItems: "3",
+    maxItems: "3",
+    items: {
+       type: "integer",
+       minimum: 0,
+       maximum: 1,
+    },
+};
+
 const insuranceRiskSchema = {
     id: "/InsuranceRisk",
     type: "object",
@@ -34,14 +46,16 @@ const insuranceRiskSchema = {
         dependents: { type: "integer" },
         house: { "$ref": "/HouseOwnershipStatus" },
         income: { type: "integer" },
-        marital_status: { type: "string" },
+        marital_status: { type: "string", enum: [ "single", "married" ] },
+        risk_questions: { "$ref": "/RiskQuestions" },
         vehicle: { "$ref": "/VehicleBasicInfos" },
     },
-    required: [ "age", "dependents", "house", "income", "marital_status", "vehicle" ],
+    required: [ "age", "dependents", "house", "income", "marital_status", "risk_questions", "vehicle" ],
 };
 
 module.exports = {
     vehicleSchema,
     houseSchema,
+    riskQuestionsSchema,
     insuranceRiskSchema,
 };
